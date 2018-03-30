32c092fb7674fbdeeefc04c32bc504838da440b6
David Kironde <david.kironde@andela.com>
2018-03-30 23:47:34 +0300

    add robots array and props for card component

--- src/cardComponent.js
+++ src/cardComponent.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
- const Card = () => {
+ const Card = ({id, name, email}) => {
   return (
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-        <h1> This is awesome"</h1>
+    <div className='bg-light-green dib br3 pa3 ma2 b--dashed grow bw2 shadow-5'>
       <img alt="robots" src='https://robohash.org/awesome?200X200'/>
-      <h1>Jane doe</h1>
-      <p>Jane.doe@email.com</p>
+      <h1>{name}</h1>
+      <p>{email}</p>
     </div>
   )
 }
---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'LGraham',
+        email: 'lg@mail.com'
+    },
+    {
+        id: 2,
+        name: 'Kityo Ben',
+        username: 'KB',
+        email: 'benk@mail.com'
+    },
+    {
+        id: 3,
+        name: 'John Bagala',
+        username: 'Bagala',
+        email: 'johnB@mail.com'
+    },
+    {
+        id: 4,
+        name: 'Lunku Jane',
+        username: 'JaneL',
+        email: 'janel@mail.com'
+    },
+    {
+        id: 5,
+        name: 'Lule Gava',
+        username: 'Gval',
+        email: 'Gval@mail.com'
+    },
+    {
+        id: 6,
+        name: 'Linda Musisi',
+        username: 'Musisi',
+        email: 'Musisi@mail.com'
+    }
+];
